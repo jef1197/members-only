12befb3bb5a7948e303d4c872e386f241212a4d4
Jef Ogues <jef.hotmail_@hotmail.com>
2022-07-20 21:35:55 -0400

    started on message page

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,8 +1,11 @@
 var async = require('async');
 var {body, validationResult} = require('express-validator');
 
+exports.messages = function(req, res, next) {
+  res.render('message_list', {title: 'Messages'});
+}
 exports.message_create_get = function(req, res, next) {
-  res.render('create_message_form', {title: 'Not done'});
+  res.render('create_message_form', {title: 'New Message'});
 }
 
 exports.message_create_post = function(req, res, next) {
